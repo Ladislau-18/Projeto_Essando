5b019cbd3d6cebc46da9aa761cf5aba5217f6128
Ladislau-18 <ladislaucabanga17@gmail.com>
2024-10-09 02:56:46 +0100

    update

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ let icon_menu = document.querySelector("#icon_mobile")
 
 icon_menu.addEventListener('click', ()=> {
 	document.querySelector("#nav").classList.toggle('ative')
+
 })
+
+let menu = document.querySelector('#menu')
